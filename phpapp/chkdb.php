eca025ff4a4548a6ac20188e9c1aef96b8e20819
aruna <aruna@apd.com>
2023-08-21 07:50:18 +0530

    commit 14

---
+++ phpapp/chkdb.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "db";
+$username = "test";
+$password = "test";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
